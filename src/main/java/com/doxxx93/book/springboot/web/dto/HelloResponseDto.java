c40c4c3dd2c7e207457b22499e5153777666da60
doxxx93 <51396905+doxxx93@users.noreply.github.com>
2022-03-18 23:57:34 +0900

    build

---
+++ src/main/java/com/doxxx93/book/springboot/web/dto/HelloResponseDto.java
@@ -0,0 +1,14 @@
+package com.doxxx93.book.springboot.web.dto;
+
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+
+}
